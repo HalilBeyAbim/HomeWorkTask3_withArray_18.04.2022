656b41083e3ce562b8a09ac64e1426a8322362af
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-17 17:03:34 +0400

    added third task with array

---
+++ homewor3test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace homewor3test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ededinizi daxil edin: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number<50)
+            {
+                Console.Write("\n uce bolunen regemler: ");
+                for (int i = 0; i < number; i++)
+                {
+                    if (i%3==0)
+                    {
+                        int numsize = (i / 3+1);
+                        int[] olacaq = new int[numsize];
+                        Console.Write(i+" " );
+                    }
+                }
+            }
+            if (number > 50&&number<100)
+            {
+                Console.Write("\n bese bolunen regemler: ");
+                for (int i = 0; i < number; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                        int numsize = (i / 5 + 1);
+                        int[] olacaq = new int[numsize];
+                        Console.Write(i+" ");
+                    }
+                }
+            }
+            if (number > 100)
+            {
+                Console.Write("\n sekkize bolunen regemler: ");
+                for (int i = 0; i < number; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        int numsize = (i / 8 + 1);
+                        int[] olacaq = new int[numsize];
+                        Console.Write(i+ " ");
+                    }
+                }
+            }
+        }
+    }
+}
